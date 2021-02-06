76f9032c93151077228a513b9ebd43908c1c55e7
Glitch (hello-webpage) <none>
2021-02-06 18:23:00 +0000

    🍦🗃 Checkpoint ./script.js:27854033/5779

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 // prints "hi" in the browser's dev tools console
 console.log("hi");
-console.log('Hello from the Web App Dev 1 lab!');
+console.log("Hello from the Web App Dev 1 lab!");
 function likeIt() {
   alert("Thanks! You're okay too");
 }
@@ -20,7 +20,10 @@ function welcomeUser() {
   let username = prompt("What's your name?");
   let welcomeUserDiv = document.getElementById("welcomeuser");
   welcomeUserDiv.style.display = "block";
-  document.getElementById('welcomeuser').innerHTML = '<p> Hello, ' + username + ', looking forward to hearing your playlists! Click this message to close it.</p>';
+  document.getElementById("welcomeuser").innerHTML =
+    "<p> Hello, " +
+    username +
+    ", looking forward to hearing your playlists! Click this message to close it.</p>";
   welcomeUserDiv.style.cursor = "pointer";
 }
 
@@ -31,14 +34,49 @@ function hideWelcome() {
   }
 }
 
-$(document).ready(function(){
-  $("#tableoutput").html("<h2 class='ui header'>" + sonatas.title + "</h2>");
-  
-  $("#tableoutput").append("<table class='ui fixef striped table'><thead><tr><th>Song</th><th>Artists</th></tr></thead><tbody>");
-  
-  for (let i in sonatas.songs){
-    $("tableoutput tr:last").after("<tr><td>" + sonatas.songs[i].title + "</td><td>" + sonatas.songs[i].artist + "</td></tr>");
+const sonatas = {
+  title: "Beethoven Sonatas",
+  songs: [
+    {
+      title: "Piano Sonata No. 3",
+      artist: "Beethoven"
+    },
+    {
+      title: "Piano Sonata No. 7",
+      artist: "Beethoven"
+    },
+    {
+      title: "Piano Sonata No. 10",
+      artist: "Beethoven"
+    }
+  ],
+  getRating() {
+    let userRating = prompt("Rate this collection (from 1 to 5 stars)");
+    $("#rating").html("You gave a rating of: " + userRating);
   }
-  
+};
+
+$(document).ready(function() {
+  $("#tableoutput").html("<h2 class='ui header'>" + sonatas.title + "</h2>");
+
+  $("#tableoutput").append(
+    "<table class='ui fixed striped table'><thead><tr><th>Song</th><th>Artist</th></tr></thead><tbody>"
+  );
+
+  for (let i in sonatas.songs) {
+    $("#tableoutput tr:last").after(
+      "<tr><td>" +
+        sonatas.songs[i].title +
+        "</td><td>" +
+        sonatas.songs[i].artist +
+        "</td></tr>"
+    );
+  }
+
   $("#tableoutput").append("</tbody></table>");
+
+  $("#tableoutput").append('<p><span id="rating"> </span></p>');
+  
+  $("#tableoutput").append('<button class="ui blue button" onclick="sonatas.getRating()"> Rate it! <i class="star icon"></i></button>');
+
 });
