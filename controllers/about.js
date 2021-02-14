1b01a2d2458285cff9328603cc1da17fb8a79802
Glitch (hello-webpage) <none>
2021-02-14 21:16:33 +0000

    🐃🕴 Checkpoint ./views/partials/menu.hbs:27854033/145 ./routes.js:27854033/227 ./controllers/about.js:27854033/507

---
+++ controllers/about.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const logger = require('../utils/logger');
+
+const about = {
+  
+  index(request, response) {
+    
+    logger.info('about rendering');
+    
+     const viewData = {
+      title: 'About Playlist App',
+    };
+    
+    response.render('about', viewData);
+  },
+};
+
+
+module.exports = about;
